6e6867303501ab85f6fe340d0a72bd4c5e3c39da
Jonas De Meyer <jonas.demeyer@profunctional.be>
2024-05-20 09:50:03 +0200

    setup shadcn + tailwindcss

---
+++ apps/admin/src/app/(site)/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+
+import '@/style/globals.css';
+
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
+export const metadata: Metadata = {
+  title: `Tobbedansen ${new Date().getFullYear()}`,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='nl'>
+      <body>{children}</body>
+    </html>
+  );
+}
